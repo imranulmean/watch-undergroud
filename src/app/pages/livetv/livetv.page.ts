a0291461ce6f9b3616be797a2f5e303f2cd4d24f
imranulmean <imranulhasan73@gmail.com>
2019-12-03 18:01:29 +0600

    india tv link added

--- src/app/pages/livetv/livetv.page.ts
+++ src/app/pages/livetv/livetv.page.ts
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class LivetvPage implements OnInit {
 
   tvChannels: any = [];
-  newsPapers:any = [];
+  indiaChannel:any = [];
   tv_newspapers:any=[];
   showSegment:number=1;
   constructor(private iab: InAppBrowser,private httpClient: HttpClient ) {
@@ -49,6 +49,29 @@ export class LivetvPage implements OnInit {
       // { channelName: '9XM MUSIC', url: 'http://9xm.in/livetv.php', logo: 'https://www.indiantelevision.com/sites/default/files/images/tv-images/2014/05/29/9xm_new.jpg' },
     ];
 
+    this.indiaChannel=[
+    // https://www.dsebd.org/images/logo.png
+    // https://www.dsebd.org/images/dse-name.jpg
+    {channelName: 'ZEE TV', url: 'https://itpolly.iptv.digijadoo.net/live/zee_tv/chunks.m3u8', logo: '',alt:"ZEE TV" },
+     { channelName: 'COLORS HD', url: "https://itpolly.iptv.digijadoo.net/live/colors_hd/chunks.m3u8", logo: '',alt:"COLORS HD" },
+     { channelName: 'STAR PLUS HD', url: 'https://itpolly.iptv.digijadoo.net/live/star_plus_hd/chunks.m3u8', logo: '',alt:"STAR PLUS HD" },
+     { channelName: 'Jugantor', url: 'https://www.jugantor.com/', logo: 'https://www.jugantor.com/templates/jugantor-v2/images/logo_main.png?v=1' },
+     { channelName: 'Ittefaq', url: 'https://www.ittefaq.com.bd/', logo: 'https://www.ittefaq.com.bd/templates/desktop-v1/images/main-logo.png' },
+     { channelName: 'Manab Zamin', url: 'http://www.mzamin.com/', logo: 'http://www.mzamin.com/asset/images/logos.png' },
+     { channelName: 'Samakal', url: 'https://samakal.com/', logo: 'https://samakal.com/assets/images/logo-bn.png' },
+     { channelName: 'Amader Shomoy', url: 'http://www.dainikamadershomoy.com/', logo: 'http://www.dainikamadershomoy.com/files/assets/img/main-logo.png' },
+     { channelName: 'Prothom Alo', url: 'https://www.prothomalo.com/', logo: 'https://paloimages.prothom-alo.com/contents/themes/public/style/images/Prothom-Alo.png' },
+     { channelName: 'Janakantha', url: 'http://web.dailyjanakantha.com/', logo: 'http://web.dailyjanakantha.com/layouts/website/assets/img/header-top.png' },
+     { channelName: 'Inqilab', url: 'https://www.dailyinqilab.com/', logo: 'https://www.dailyinqilab.com/includes/themes/dailyinqilab/images/logo.png' },
+     { channelName: 'Bhorer Kagoj', url: 'https://www.bhorerkagoj.com/', logo: 'https://www.w3newspapers.com/bangladesh/images/bhorerkagoj.png' },
+     {channelName: 'Daily Star', url: 'https://www.thedailystar.net/', logo: 'https://assetsds.cdnedge.bluemix.net/sites/all/themes/tds/logo.svg' },
+     {channelName: 'Observer', url: 'https://www.observerbd.com/', logo: 'https://www.observerbd.com/files/logo.jpg' },
+     {channelName: 'Bangladesh Today', url: 'http://thebangladeshtoday.com/', logo: 'https://thebangladeshtoday.com/wp-content/uploads/2018/06/TBT-Logo-Transparent-1-1.png' },
+     {channelName: 'bdnews24.com', url: 'https://bangla.bdnews24.com/', logo: 'https://d30fl32nd2baj9.cloudfront.net/media/2013/01/04/logo1.png1/BINARY/logo1.png' },
+     {channelName: 'banglanews24.com', url: 'https://www.banglanews24.com', logo: 'https://www.banglanews24.com/media/imgAll/2016October/bg/banglanews_logo20180725112204.jpg' },
+     {channelName: 'dailystockbangladesh', url: 'https://www.dailystockbangladesh.com', logo: '../../../assets/img/news-source-icons/dailystock.png' },
+     
+    ];
    this.tv_newspapers=this.tvChannels;
   }
 
@@ -70,6 +93,38 @@ export class LivetvPage implements OnInit {
       //       console.log("Failed to open URL via Android Intent.")
       //     });    
   }
+  segmentChanged(ev: any) {
+     console.log('Segment changed', ev.detail.value);
+     this.showSegment=ev.detail.value;
+     this.showSegment==1 ? document.getElementById("ionContentLive").style.setProperty('--background','#f1f1f1' ) : document.getElementById("ionContentLive").style.setProperty('--background','#ffffff' );
+     this.goToSegment(); 
+  }
+
+  goToSegment(){
+   this.tv_newspapers=this.showSegment==1 ?  this.tvChannels : this.indiaChannel;       
+    let nextButtonNumber:number = this.showSegment;
+    document.getElementById(String(nextButtonNumber)).click();
+    let buttonWidth = document.getElementById(String(nextButtonNumber)).offsetWidth;
+    document.getElementById('segment').scrollLeft = nextButtonNumber * buttonWidth - buttonWidth; 
+  } 
+  swipeLeftPress($event) {
+   this.showSegment=Number(this.showSegment)+1;
+   if(this.showSegment>2) {
+    this.showSegment=1;
+   }
+   this.goToSegment();
+   
+  }
+
+ 
+  swipeRightPress($event) {
+   this.showSegment=Number(this.showSegment)-1;
+   if(this.showSegment<1) {
+    this.showSegment=2;
+   }
+   this.goToSegment();
+  }
+
   ngOnInit() {
   }
 
